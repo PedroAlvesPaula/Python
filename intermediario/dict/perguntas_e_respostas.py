b9989becf6eae6984707ac74041a45fa9ef373b7
Pedro <pedropaula2325p@gmail.com>
2023-11-21 17:16:15 -0300

    adcionando perguntas_e_respostas, exercicio sobre tipo de dados

---
+++ intermediario/dict/perguntas_e_respostas.py
@@ -0,0 +1,51 @@
+def imprimir_opcoes(posicao):
+    print(perguntas[posicao]['Pergunta'])
+
+    for indice, opcao in enumerate(perguntas[posicao]['Opções']):
+        print(f'({indice}) {opcao}')
+
+def verificar_resposta_usuario(posicao, resposta):
+    for indice, opcao in enumerate(perguntas[posicao]['Opções']):
+        if int(resposta) == indice :
+            print("Resposta correta!!")
+            return True
+
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
+
+continuar = True
+range = 0
+
+while continuar == True:
+
+    imprimir_opcoes(range)
+    resposta_usuario = input("Digite o indice da opção correta: ")
+    resposta_verificada = verificar_resposta_usuario(range, resposta_usuario)
+
+    if resposta_verificada == True:
+        range += 1
+
+        if range == len(perguntas):
+            continuar = False
+        continue
+    else:
+        print("Resposta errada, tente novamente!")
+        continue
+
+print("Muito bem, ate mais")
+    
